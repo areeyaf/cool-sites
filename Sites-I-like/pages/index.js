cffb266db88ad9cd918334291841a5f73d5b0a69
Areeya <137256295+areeyaf@users.noreply.github.com>
2024-11-18 13:00:04 +0800

    Update index.js

--- Sites-I-like/pages/index.js
+++ Sites-I-like/pages/index.js
@@ -171,7 +171,7 @@ function Home(props) {
 export async function getServerSideProps() {
   const Airtable = require("airtable");
   const base = new Airtable({ apiKey: process.env.COOL_SITES }).base(
-    process.env.AIRTABLE_BASE_ID
+    process.env.appLJdDRT9kPihSZN
   );
   const table = base("Cool sites");
   let records = await table
